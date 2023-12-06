0cce690f0337e9dee51adcfa64e781c2e3dee1e8
Yeremi74 <yyeremi15@gmail.com>
2023-12-06 01:19:15 -0400

    fix link

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -25,7 +25,7 @@ const Search = ({ busqueda, max, setIsFocused, isFocused }) => {
       ) : data ? (
         data.slice(0, max).map(item => (
           <Link
-            to={`eal-eyes-strapi.onrender.com/product/${item.id}`}
+            to={`https://real-eyes-strapi.onrender.com/product/${item.id}`}
             className='result_buscador'
             onClick={handleSearchOther}
             key={item.id}
