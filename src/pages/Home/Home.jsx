70171ab90c656c4da148165d9653217ed08ade76
Yeremi74 <yyeremi15@gmail.com>
2023-12-10 22:05:28 -0400

    main catalog section added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,15 +11,23 @@ const Home = () => {
         <p>Explore your style</p>
       </div>
       <List type='top selles' />
-      <div className='gender_container'>
-        <Link to='/products/mujeres/1' className='gender_section women'>
-          <div className='img'></div>
-          <p>Mujeres</p>
-        </Link>
-        <Link to='/products/hombres/2' className='gender_section men'>
-          <div className='img'></div>
-          <p>Hombres</p>
-        </Link>
+      <div className='sections_container'>
+        <div className='gender_container'>
+          <Link to='/products/mujeres/1' className='section women'>
+            <div className='img'></div>
+            <p>Mujeres</p>
+          </Link>
+          <Link to='/products/hombres/2' className='section men'>
+            <div className='img'></div>
+            <p>Hombres</p>
+          </Link>
+        </div>
+        <div className='gender_container'>
+          <Link to='/catalogo/*' className='section catalogo'>
+            <div className='img'></div>
+            <p>Catalogo</p>
+          </Link>
+        </div>
       </div>
       <List type='featured' />
     </div>
